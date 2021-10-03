06a626f401289e52744707289a95f768afbf5303
Zachary Leinweber <zleinweb@gmail.com>
2021-10-03 16:56:00 +0000

    Simple prod config with asset management.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import _ from "lodash";
-
 function component() {
   const element = document.createElement("div");
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpack", "zach"], " ");
+  element.innerHTML = "Hello World!";
+  element.classList.add("hello");
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,45 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    mode: 'production'
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+        ]
+    }
 }
