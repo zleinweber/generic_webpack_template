84826ce4ded2077e55be289b899e2d1aebaf63be
Zachary Leinweber <zleinweb@gmail.com>
2021-10-10 03:58:59 +0000

    Add development server.

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,21 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: "./src/index.js",
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    compress: true,
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Development',
     })
   ],
   module: {
